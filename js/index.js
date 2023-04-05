cce6b02759bcaf70094c720c369a7827589f7bc1
muis <128585576+kickvandenboom@users.noreply.github.com>
2023-04-05 10:19:32 +0200

    final version (probably)

--- js/index.js
+++ js/index.js
@@ -2,16 +2,32 @@ const mainCharacter = document.getElementById("mainCharacter");
 const gameWindow = document.getElementById("gameWindow");
 const offsetCharacter = 16;
 
+const humblemanAudio = document.getElementById("humblemanAudio");
+const characterAudio = document.getElementById("characterAudio");
+
+const title = document.getElementById("title")
+
 let playerHasAxe = false;
 let treeChoppedDown = false;
 let playerHasKey = false;
-let playerIsInside = false;
+
+let currentAreaID = 1; //GAME AREAS: 1=OUTSIDE 2=HUMBLEROOM
+
 
 let awaytPlayerConfirm = false;
 
-let awaitYesOrNo = false;
-
 document.getElementById('ground').setAttribute('draggable', false);
+
+function onLoad(){
+    let hilariousValue = Math.random() * 100;
+    console.log(hilariousValue);
+
+    if(hilariousValue < 11){
+
+        document.getElementById("what").classList.toggle("humbleDoorInvis");
+        humbleroomSpawn = true;
+    }
+}
 
 gameWindow.onclick = function (e) {
 
@@ -23,103 +39,129 @@ gameWindow.onclick = function (e) {
 
     console.log(e.target.id);
 
-    switch(e.target.id){
-        case "door0" :
-        case "axe" : //Axe pickup
-            if(!playerHasAxe){
-                playerHasAxe = true;
-                document.getElementById("title").innerHTML = ("You found an axe and picked it up.") //Updates the title, wanted to make seperate function for it but couldnt figure out how to do function paremeters in js
-                document.getElementById("axe").remove();
-                document.getElementById("door0").remove();
+    switch(currentAreaID){
+        case 1: //OUTSIDE ROOM SWITCHES
+            switch(e.target.id){
+                case "door0" :
+                case "axe" : //Axe pickup
+                    if(!playerHasAxe){
+                        playerHasAxe = true;
+                        updateTitle("You found an axe and picked it up.", characterAudio)
+                        document.getElementById("axe").remove();
+                        document.getElementById("door0").remove();
+                    }
+                    break;
+                case "door1": case "door3": case "door4": //Wrong trees
+                    if(playerHasAxe){
+                        updateTitle("The tree is too big to chop down.", characterAudio)
+                    }else{
+                        updateTitle("Its a large tree.", characterAudio)
+                    }
+                    break;
+                case "door5": //Right tree
+                    if(playerHasAxe){
+                        playerHasKey = true;
+                        updateTitle("You chopped down the tree and a small key fell out.", characterAudio)
+                        document.getElementById("door5").remove();
+        
+                        document.getElementById("ground").src="Img/map_2.png"; //Switch background to new image without tree
+                        document.getElementById("foreGround").src="Img/map_2_foreground.png"; //Switch foreground to new image without tree
+                    }else{
+                        updateTitle("Its a medium sized tree.", characterAudio)
+                    }
+                    break;
+                case "door2": //Wrong door
+                    if(playerHasKey){
+                        updateTitle("You hear a voice from behind you wisper `wrong door`.", characterAudio)
+                    }else{
+                        updateTitle("The door is locked.", characterAudio)
+                    }
+                    break;
+                case "what": //Humble room door
+                    goToHumbleRoom();
+                    break;
+                case "door6": //Right door
+                    if(awaytPlayerConfirm){
+                        playerIsInside = true;
+                        goInside();
+                    }else{
+                        if(playerHasKey){
+                            updateTitle("You swear you have never seen this door before, go in? (Click again to go in)", characterAudio)
+                            awaytPlayerConfirm = true;
+                        }else{
+                            updateTitle("Theres nothing here", characterAudio)
+                        }
+                    break;
+                    }
             }
             break;
-        case "door1": case "door3": case "door4": //Wrong trees
-            if(playerHasAxe){
-                document.getElementById("title").innerHTML = ("The tree is too big to chop down.")
-            }else{
-                document.getElementById("title").innerHTML = ("Its a large tree.")
+        case 2: //HUMBLE ROOM SWITCHES
+            switch(e.target.id){
+                case "humbleMan":
+                    document.getElementById("humbleMan").src="Img/humble_man_disapear.gif";
+                    playSound(humblemanAudio);
+                    setTimeout(function(){
+                        humbleManDissapear();
+                    }, 500);
+                    break;
             }
             break;
-        case "door5": //Right tree
-            if(playerHasAxe){
-                playerHasKey = true;
-                document.getElementById("title").innerHTML = ("You chopped down the tree and a small key fell out.")
-                document.getElementById("door5").remove();
+    }
 
-                document.getElementById("ground").src="Img/map_2.png"; //Switch background to new image without tree
-            }else{
-                document.getElementById("title").innerHTML = ("Its a medium sized tree.")
-            }
-            break;
-        case "door2": //Wrong door
-            if(playerHasKey){
-                document.getElementById("title").innerHTML = ("You hear a voice from behind you wisper `wrong door`.")
-            }else{
-                document.getElementById("title").innerHTML = ("The door is locked.")
-            }
-            break;
-        case "door6": //Right door
-            if(awaytPlayerConfirm){
-                playerIsInside = true;
-                updateBackground();
-            }else{
-                if(playerHasKey){
-                    document.getElementById("title").innerHTML = ("You swear you have never seen this door before, go in? (Click again to go in)")
-                    awaytPlayerConfirm = true;
-                }else{
-                    document.getElementById("title").innerHTML = ("Theres nothing here")
-                }
-                break;
-            }
+
+}
+
+function playSound(targetSound){
+    targetSound.play();
+}
+
+function updateTitle(newTitle, targetSound){
+    targetSound.play();
+    title.innerHTML = (newTitle)
+}
+
+function goToHumbleRoom(){
+    currentAreaID = 2; //Update current room ID
+
+    if(!playerHasAxe){
+        document.getElementById("axe").remove();
+    }
+
+    document.getElementById("ground").src="Img/humbleroom.png"; //Switch background to humble room image
+    document.getElementById("foreGround").remove();
+    document.getElementById("what").remove();
+
+    document.getElementById("humbleMan").classList.toggle("humbleDoorInvis");
+
+    title.innerHTML = (" ")
+
+}
+
+function humbleManDissapear(){
+    document.getElementById("humbleMan").remove();
+
+    if(playerHasAxe){
+        document.getElementById("ground").src="Img/map_fallofthelastwizard.png";
+        updateTitle("The strange man took you to a paralell universe, there seems to be no way out.", characterAudio)
     }
 }
 
+function goInside(){
+    document.getElementById("foreGround").remove();
+    document.getElementById("ground").remove();
+    document.getElementById("what").remove();
+    document.getElementById("title").remove();
+    document.getElementById("mainCharacter").remove();
 
-function updateBackground(){
-    if(!playerIsInside){
-        document.body.style.backgroundImage = "url(../Img/island_background.png)";
-        console.log("player went outside")
-    }else{
-        document.body.style.backgroundImage = "url(../Img/void.png)";
-        document.body.style.backgroundColor = "black";
+    playSound(endingAudio)
 
-        document.getElementById("title").remove();
-        document.getElementById("ground").remove();
-        document.getElementById("mainCharacter").remove();
-
-        finaleSequence();
-
-        console.log("player went inside")
-    }
+    setTimeout(function(){
+        endingTextAppear();
+    }, 6500);
 }
 
-function finaleSequence(){
-    setTimeout(() => {
-        finale1();
-    }, 2000);
+function endingTextAppear(){
+    document.getElementById("endtext").classList.toggle("humbleDoorInvis");
 }
 
-function finale1(){
-    document.getElementById("finale1").style.color = "white";
-    setTimeout(() => {
-        finale2();
-    }, 2000)
-}
-
-function finale2(){
-    document.getElementById("finale2").style.color = "white";
-    setTimeout(() => {
-        finale3();
-    }, 3000)
-}
-
-function finale3(){
-    document.getElementById("finale3").style.color = "white";
-    setTimeout(() => {
-        finale4();
-    }, 5000)
-}
-
-function finale4(){
-    document.getElementById("finale4").style.color = "white";
-}
+window.onload = onLoad;
